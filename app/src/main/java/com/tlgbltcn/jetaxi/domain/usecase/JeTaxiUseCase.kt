0191643a2d071cc293ea98e20faf5b3612f48448
Bolatcan, Tolga <tolgabolatcan@hotmail.com>
2021-11-25 19:04:45 +0300

    The wrong class has been changed.

--- app/src/main/java/com/tlgbltcn/jetaxi/domain/usecase/JeTaxiUseCase.kt
+++ app/src/main/java/com/tlgbltcn/jetaxi/domain/usecase/JeTaxiUseCase.kt
@@ -1,8 +1,8 @@
 package com.tlgbltcn.jetaxi.domain.usecase
 
-import com.tlgbltcn.jetaxi.data.model.TaxisDataModel
 import com.tlgbltcn.jetaxi.data.repository.JeTaxiRepository
 import com.tlgbltcn.jetaxi.feature.map.TaxisState
+import com.tlgbltcn.jetaxi.ui.model.Taxis
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -10,7 +10,7 @@ class JeTaxiUseCase @Inject constructor(
     private val repository: JeTaxiRepository,
     // since no need a logic, no need dispatcher in this case.
     // @IoDispatcher private val dispatcher: CoroutineDispatcher
-) : UseCase<TaxisDataModel, TaxisState> {
+) : UseCase<Taxis, TaxisState> {
 
     override suspend fun execute(): Flow<TaxisState> {
         // no need a logic in this case.
